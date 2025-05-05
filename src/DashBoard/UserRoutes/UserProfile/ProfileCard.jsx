06deb33bc647d5ca80efd15ffd928aefc199b48b
ActiveShayun <wevdeveloper0477gmail.com>
2025-05-06 00:05:01 +0600

    customise user profile

--- src/DashBoard/UserRoutes/UserProfile/ProfileCard.jsx
+++ src/DashBoard/UserRoutes/UserProfile/ProfileCard.jsx
@@ -1,34 +1,20 @@
 import React from 'react';
 import { MdMarkEmailRead, MdOutlinePhoneInTalk } from 'react-icons/md';
 import SectionTitle from '../../../Shared/SectionTitle/SectionTitle';
-import UseAuth from '../../../AuthProvider/UseAuth';
-import { FaCamera } from "react-icons/fa";
-import { useQuery } from '@tanstack/react-query';
-import AxiosSecure from '../../../UseHooks/AxiosSecure/AxiosSecure';
 
-const Profile = () => {
-    const { user } = UseAuth()
-    const axiosSecure = AxiosSecure()
 
-    const { data: updateProfile = [] } = useQuery({
-        queryKey: ['profile'],
-        queryFn: async () => {
-            const { data } = await axiosSecure.get(`/single-User?email=${user?.email}`)
-            console.log('userInfo', data);
-            return data
-        }
 
-    })
+const ProfileCard = () => {
     return (
         <div className='min-h-screen flex flex-col items-center justify-center relative'>
             <SectionTitle heading={'Your Vip Profile'} />
             <div className='bg-slide-1 w-9/12 h-[200px] bg-cover bg-no-repeat p-4  rounded-md 
-         '>
+     '>
                 <div className='w-[150px] h-[150px] object-cover rounded-full  z-70 relative top-28'>
                     <img
-                        src={updateProfile?.userPhoto} alt=""
+                        src={user?.photoURL} alt=""
                         className='w-[150px] h-[150px] object-cover rounded-full
-                        '
+                    '
                     />
                     <div className='relative -top-10 -right-[50%]'>
                         <input
@@ -38,10 +24,10 @@ const Profile = () => {
                     </div>
                 </div>
                 <div className='relative top-32'>
-                    <h2 className='font-semibold'>Name - {updateProfile?.name}</h2>
+                    <h2 className='font-semibold'>Name - {user?.displayName}</h2>
                     <h2 className='font-semibold flex gap-2 items-center'>
                         <span><MdMarkEmailRead /></span>
-                        Email - {updateProfile?.email}
+                        Email - {user?.email}
                     </h2>
                     <h2 className='font-semibold flex gap-2 items-center'>
                         <span><MdOutlinePhoneInTalk /></span>
@@ -59,4 +45,4 @@ const Profile = () => {
     );
 };
 
-export default Profile;
+export default ProfileCard;
